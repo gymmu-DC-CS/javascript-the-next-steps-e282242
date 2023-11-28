884e8beea514a63491eca18b50bcc764f9baf9da
e282242 <e282242@sbl.ch>
2023-11-28 16:31:53 +0100

    Exercise1

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement )
     result.push(currentElement)
   }
   return result.join("")
 }
+

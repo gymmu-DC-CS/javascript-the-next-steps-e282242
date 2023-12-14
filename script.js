49cfed6cd2159ce30fbb59e96e8729c393bc4f6a
e282242 <e282242@sbl.ch>
2023-12-14 15:42:47 +0100

    Number_6

--- script.js
+++ script.js
@@ -21,9 +21,50 @@ export function exercise02(args) {
   return result.join("")
 }
 
+// export function exercise03(args) {
+//   const input = args
+//   const result = []
+//   let numOfChars = currentElement.match(/[e]/g).length
+//   HTMLFormControlsCollection.log(numOfChars)
+// }
+
 export function exercise03(args) {
+  let input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+      result++
+    }
+  }
+  return result
+}
+
+export function exercise05(args) {
   const input = args
-  const result = []
-  let numOfChars = currentElement.match(/[e]/g).length
-  HTMLFormControlsCollection.log(numOfChars)
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (currentElement >= 65 && currentElement <= 90) {
+      return true
+    }
+  }
+  return false
+}
+
+export function exercise04(args) {
+  let input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (
+      currentElement === 32 ||
+      (currentElement === 33 && currentElement === s)
+    ) {
+      result++
+    }
+  }
+  return result + 1
 }
